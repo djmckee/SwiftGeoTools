a4a13eb91e7a4c85c0204b23aa7d4b89a9c8cf90
Dylan McKee <dylan@djmckee.co.uk>
2015-11-09 00:31:32 +0000

    Initial Commit

---
+++ SwiftGeoTools/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftGeoTools
+//
+//  Created by Dylan McKee on 09/11/2015.
+//  Copyright © 2015 djmckee. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
